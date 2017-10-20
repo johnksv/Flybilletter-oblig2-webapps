cedf10f5035bbdbe945f03dc3d80b75a8e68c096
Jesper Nylend <jesper.nylend@gmail.com>
2017-10-20 09:45:46 +0200

    Gjør klar til å skrive feilmeldinger til fil.

---
+++ DAL/DALsetup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DALsetup
+    {
+        public static void logFeilTilFil(string metodeNavn, Exception e)
+        {
+            //For å sende med metodenavn, send denne koden som parameter: "System.Reflection.MethodBase.GetCurrentMethod().Name"
+            System.IO.StreamWriter writer = new System.IO.StreamWriter(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, @"..\DAL\flybilletter-log.txt"), true);
+            writer.WriteLine(DateTime.Now + ": " + metodeNavn + " - " + e.InnerException);
+        }
+    }
+}
