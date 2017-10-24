11f53f6e5a761c6cbb82da40c3851463225b1414
Trygve Vang <trygve.n.vang@gmail.com>
2017-10-24 09:34:26 +0200

    Ordner view for login

--- DAL/Stub/DBAdminStub.cs
+++ DAL/Stub/DBAdminStub.cs
@@ -4,10 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Flybilletter.DAL.DBModel;
 
 namespace Flybilletter.DAL.Stub
 {
     public class DBAdminStub : IDBAdmin
     {
+        public DBAdmin Hent(string username)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
