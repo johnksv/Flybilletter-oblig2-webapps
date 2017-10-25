d2c2304e966cfe816d01a28c081538aaa43d1a1f
John Kasper Svergja <john.k.svergja@gmail.com>
2017-10-25 21:27:23 +0200

    Legger til filer for liste ruter

--- Model/DomeneModel/Rute.cs
+++ Model/DomeneModel/Rute.cs
@@ -8,8 +8,8 @@ namespace Flybilletter.Model.DomeneModel
     public class Rute
     {
         public int ID { get; set; }
-        public virtual Flyplass Fra { get; set; }
-        public virtual Flyplass Til { get; set; }
+        public Flyplass Fra { get; set; }
+        public Flyplass Til { get; set; }
         public double BasePris { get; set; } //faktor for å regne ut total pris for turen
 
     }
