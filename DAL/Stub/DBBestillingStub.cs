90479510c5587cd184b96dfffd98ea995eb8141b
Jesper Nylend <jesper.nylend@gmail.com>
2017-10-27 14:14:54 +0200

    Legger til test for SlettBestilling

--- DAL/Stub/DBBestillingStub.cs
+++ DAL/Stub/DBBestillingStub.cs
@@ -15,6 +15,46 @@ namespace Flybilletter.DAL.Stub
             new Bestilling()
             {
                 Referanse ="ARP123"
+            },
+            new Bestilling()
+            {
+                Referanse = "AAA123",
+                FlygningerTur = new List<Flygning>(){
+                    new Flygning() //Kunde kan slette
+                {
+                    AvgangsTid = new DateTime(2018,01,01,00,00,00)
+                } },                
+               Bestillingstidspunkt = new DateTime(2017,10,20,12,00,00) //Kunde kan ikke slette
+            },
+            new Bestilling()
+            {
+                Referanse = "AAB123",
+                FlygningerTur = new List<Flygning>(){
+                    new Flygning() //Kunde kan slette
+                {
+                    AvgangsTid = new DateTime(2018,01,01,00,00,00)
+                } },
+                Bestillingstidspunkt = new DateTime(2018,11,27,12,00,00) //Kunde kan slette
+            },
+            new Bestilling()
+            {
+                Referanse = "ABB123",
+                FlygningerTur = new List<Flygning>(){
+                    new Flygning() //Kunde kan ikke slette
+                {
+                    AvgangsTid = new DateTime(2016,01,01,00,00,00)
+                } },
+                Bestillingstidspunkt = new DateTime(2018,11,27,12,00,00) //Kunde kan slette
+            },
+            new Bestilling()
+            {
+                Referanse = "ABC123",
+                FlygningerTur = new List<Flygning>(){
+                    new Flygning() //Kunde kan ikke slette
+                {
+                    AvgangsTid = new DateTime(2016,01,01,00,00,00)
+                } },
+                Bestillingstidspunkt = new DateTime(2016,11,27,12,00,00) //Kunde kan ikke slette
             }
         };
 
@@ -41,7 +81,15 @@ namespace Flybilletter.DAL.Stub
 
         public bool Slett(string referanse)
         {
-            throw new NotImplementedException();
+            var bestilling = bestillinger.FirstOrDefault(best => best.Referanse == referanse);
+            if((DateTime.Now - bestilling.Bestillingstidspunkt) < new TimeSpan(48, 0, 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
